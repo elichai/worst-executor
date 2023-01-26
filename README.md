8856bcf7fff3b0cc231b9d9a174522835d1c5de0
Elichai Turkel <elichai.turkel@gmail.com>
2023-01-26 13:05:28 +0200

    Add documentation and a full TCP-server

---
+++ README.md
@@ -0,0 +1,88 @@
+# Worst Executor
+[![Build status](https://github.com/elichai/worst-executor/workflows/ci/badge.svg)](https://github.com/elichai/worst-executor/actions)
+[![Latest version](https://img.shields.io/crates/v/worst-executor.svg)](https://crates.io/crates/worst-executor)
+![License](https://img.shields.io/crates/l/worst-executor.svg)
+[![dependency status](https://deps.rs/repo/github/elichai/worst-executor/status.svg)](https://deps.rs/repo/github/elichai/worst-executor)
+
+The simplest async executor possible. <br>
+This crate provides a single function, `block_on`, which takes a future and
+blocks the current thread until the future is resolved. <br>
+The way it works is by "spin-looping" over the `poll` method until it is ready.
+
+The nice thing about this is that it optimizes very well,
+ for example `worst_executor::block_on(async { 42 })` compiles to a single `mov` instruction. <br>
+
+The bad thing about this is that it does not actually do any scheduling, meaning that if you
+wait on a future that never resolves, your program will hang. which is why you should probably not use this.
+
+Note that because of its simplicity, the library only uses `core` and does not require `std` or `alloc`
+nor does it have any dependencies, and is literally 16 lines of code.
+
+* [Documentation](https://docs.rs/worst-executor)
+
+## Usage
+
+Add this to your `Cargo.toml`:
+
+```toml
+[dependencies]
+worst-executor = "0.1"
+```
+Then in your main.rs:
+
+```rust
+
+fn main() {
+    worst_executor::block_on(async {
+        // Your async code goes here.
+    });
+}
+```
+
+### Use cases
+Say you're using a library which returns a future, but you know it does no I/O, 
+and you want to use it in a synchronous context without bringing in a full async runtime like tokio/smol/async-std <br>
+You can use `worst_executor::block_on` to block the current thread until the future is resolved.
+
+Another scenario can be that you want to run an "event loop" in a single thread using `async` rust.
+so you can use the `core::future::join` macro together with the `futures::select` macro
+to handle the control flow of your program, while always running on a single thread and never yielding.
+
+### Single threaded tcp server
+```rust
+use async_net::{TcpListener, TcpStream};
+use futures::{stream::FuturesUnordered, AsyncReadExt, AsyncWriteExt, StreamExt};
+
+async fn handle_connection(mut stream: TcpStream) -> Option<TcpStream> {
+    let mut buf = [0u8; 1024];
+    let n = match stream.read(&mut buf).await {
+        Ok(n) if n == 0 => return Some(stream),
+        Ok(n) => n,
+        Err(e) => {
+            eprintln!("failed to read from the socket {e:?}");
+            return None;
+        }
+    };
+    // Write the data back
+    stream.write_all(&buf[0..n]).await
+            .map_err(|e| eprintln!("failed to write to the socket {e:?}"))
+            .map(|()| stream)
+            .ok()
+}
+
+worst_executor::block_on(async {
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    let mut connection_handlers = FuturesUnordered::new();
+    // This stream is inifinite so same to call fuse.
+    let mut listener = listener.incoming().fuse();
+    loop {
+        futures::select! {
+            new_connection = listener.select_next_some() => connection_handlers.push(handle_connection(new_connection?)),
+            socket = connection_handlers.select_next_some() => 
+                if let Some(socket) = socket {
+                    connection_handlers.push(handle_connection(socket));
+                },
+        }
+    }
+})
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,74 @@
+//! The worst async executor you could think of.
+//!
+//! This crate provides a single function, `block_on`, which takes a future and
+//! blocks the current thread until the future is resolved.
+//! The way it works is by "spin-looping" over the `poll` method until it is ready.
+//!
+//! The nice thing about this is that it optimizes very well,
+//! for example `worst_executor::block_on(async { 42 })` compiles to a single `mov` instruction.
+//!
+//! The bad thing about this is that it does not actually do any scheduling, meaning that if you
+//! wait on a future that never resolves, your program will hang. which is why you should probably not use this.
+//!
+//! Note that because of its simplicity, the library only uses `core` and does not require `std` or `alloc`
+//! and is literally 16 lines of code.
+//!
+//! This can become more useful using the `core::future::join` macro
+//! which allows you to wait on multiple futures at once (each time polling a different future).
+//!
+//! Currently this library requires rust *nightly* for the `pin` macro, the `join` macro(used in tests) and the `const_waker` feature (required for complete optimization of `block_on`)
+//!
+//! # Examples
+//! ```rust
+//! use worst_executor::block_on;
+//!
+//! let val = block_on(async { 42 });
+//! assert_eq!(val, 42);
+//! ```
+//!
+//! ```rust
+//! #![feature(future_join)]
+//! use worst_executor::block_on;
+//! use core::future::join;
+//!
+//!  let (sender, receiver) = async_channel::bounded(1);
+//!  block_on(async {
+//!     let send_20_fut = async {
+//!         for i in 0..20 {
+//!             sender.send(i).await.unwrap();
+//!         }
+//!     };
+//!     let recv_20_fut = async {
+//!         for i in 0..20 {
+//!             assert_eq!(receiver.recv().await.unwrap(), i);
+//!         }
+//!     };
+//!     join!(send_20_fut, recv_20_fut).await;
+//!  });
+//! ```
+
 #![cfg_attr(not(test), no_std)]
-#![feature(waker_getters, const_waker)]
+#![feature(const_waker)]
 #![cfg_attr(test, feature(future_join))]
 
 use core::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
 use core::{future::Future, ptr::null};
 use core::{hint, pin::pin};
 
+/// Runs the given future to completion on the current thread.
+/// This function will block the current thread until the future is resolved.
+/// The way it works is by "spin-looping" over the `poll` method until it is ready.
+///
+/// It will not do any scheduling, nor will it launch any threads.
+///
+/// # Examples
+/// ```rust
+/// use worst_executor::block_on;
+/// block_on(async {
+///    println!("Hello, world!");
+/// })
+/// ```
+///
 pub fn block_on<F: Future>(f: F) -> F::Output {
     static WAKER: Waker = {
         const RAW_WAKER: RawWaker = RawWaker::new(
@@ -29,7 +92,11 @@ mod tests {
     use crate::block_on;
     use core::future::join;
     use core::{future, pin::pin, task::Poll};
-    use futures_lite::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
+    use futures::{
+        stream::FuturesUnordered, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt, FutureExt,
+        StreamExt,
+    };
+
     #[test]
     fn test_block_on_trivial() {
         assert_eq!(block_on(async { 42 }), 42);
@@ -173,5 +240,61 @@ mod tests {
             };
             join!(send_20_fut, recv_20_fut).await;
         });
+    }
+
+    #[test]
+    fn test_tcp_server() {
+        use async_net::{TcpListener, TcpStream};
+
+        async fn handle_connection(mut stream: TcpStream) -> Option<TcpStream> {
+            let mut buf = [0u8; 1024];
+            let n = match stream.read(&mut buf).await {
+                Ok(n) if n == 0 => return Some(stream),
+                Ok(n) => n,
+                Err(e) => {
+                    eprintln!("failed to read from the socket {e:?}");
+                    return None;
+                }
+            };
+            // Write the data back
+            stream.write_all(&buf[0..n]).await.map_err(|e| eprintln!("failed to write to the socket {e:?}")).map(|()| stream).ok()
+        }
+        let localhost = "127.0.0.1";
+        let listener = crate::block_on(TcpListener::bind((localhost, 0))).unwrap();
+        let port = listener.local_addr().unwrap().port();
+
+        let res = block_on(async move {
+            let mut readers = futures::future::join_all((0..10).map(|i| async move {
+                let mut stream = TcpStream::connect((localhost, port)).await.unwrap();
+                let data =
+                    format!("Hello, world! from my async executor in TCP stream number: {i}");
+                stream.write_all(data.as_bytes()).await.unwrap();
+                let mut buf = vec![0u8; data.len()];
+                stream.read_exact(&mut buf).await.unwrap();
+                assert_eq!(buf, data.as_bytes());
+                i
+            }))
+            .fuse();
+            // This stream is inifinite so same to call fuse.
+            let mut connection_handlers = FuturesUnordered::new();
+            let mut listener = listener.incoming().fuse();
+            loop {
+                futures::select! {
+                    new_connection = listener.select_next_some() => {
+                        connection_handlers.push(handle_connection(new_connection.unwrap()));
+                    },
+                    socket = connection_handlers.select_next_some() => {
+                        if let Some(socket) = socket {
+                            connection_handlers.push(handle_connection(socket));
+                        }
+                    },
+                    result = readers => {
+                        break result
+                    }
+                }
+            }
+        });
+
+        assert_eq!(res, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
     }
 }
